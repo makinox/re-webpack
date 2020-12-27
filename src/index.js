b47af49fe4f74ee39c709e127a8237f56b547ad1
Jesus Bossa <x@jesusbossa.dev>
2020-12-26 22:32:38 -0500

    [ADD] Code splitting

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
-import _ from 'lodash';
-import './style.css';
-import printMe from './print.js';
-
-function component() {
+async function getComponent() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
+  const { default: _ } = await import('lodash');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
   return element;
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
   },
   devtool: 'inline-source-map',
   devServer: {
